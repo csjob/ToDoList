437da4c121ef4696fc6d51f18ded54858999912b
C S Job <csjob78@gmail.com>
2021-11-26 18:04:17 +0530

    Get Started ⌛ and Login design completed ✅

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TODO LIST' });
 });
 
+
+router.get('/signup-in', function(req, res, next) {
+  
+  res.render('signup-in', { title: 'TODO LIST' });
+});
+
+
 module.exports = router;
